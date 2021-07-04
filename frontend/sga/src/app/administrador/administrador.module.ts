bf77d1d80d62455f61d2bcebf8c66b79795b9249
stteffano13 <tefo.aguayo@gmail.com>
2021-07-04 12:15:01 -0500

    listo para empezar el backend

--- frontend/sga/src/app/administrador/administrador.module.ts
+++ frontend/sga/src/app/administrador/administrador.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AdministradorRoutingModule } from './administrador-routing.module';
 import { MenuAdministradorComponent } from './menu-administrador/menu-administrador.component';
 import { RegistroEstudianteComponent } from './registro-estudiante/registro-estudiante.component';
-
+import {FormsModule} from "@angular/forms";
 @NgModule({
   declarations: [MenuAdministradorComponent, RegistroEstudianteComponent],
   imports: [
     CommonModule,
-    AdministradorRoutingModule
+    AdministradorRoutingModule,
+    FormsModule
   ]
 })
 export class AdministradorModule { }
--- frontend/sga/src/app/administrador/registro-estudiante/registro-estudiante.component.ts
+++ frontend/sga/src/app/administrador/registro-estudiante/registro-estudiante.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Estudiante } from "../../../models/estudiante";
 @Component({
   selector: 'app-registro-estudiante',
   templateUrl: './registro-estudiante.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEstudianteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public estudiante_register: Estudiante;
+  public clase_ojoEstudiante = 'fa fa-eye fa-lg';
+  constructor() { 
+    this.estudiante_register = new Estudiante("", "", "", "", "", "", "", "");
   }
+  
+  ngOnInit(){
 
+  }
 }
