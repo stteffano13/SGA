a71629e3f9095decba1c8ea5d77234cca2d6d0b1
stteffano13 <tefo.aguayo@gmail.com>
2021-07-05 11:08:49 -0500

    listado estudiante

--- frontend/sga/src/app/administrador/listado-estudiantes/listado-estudiantes.component.ts
+++ frontend/sga/src/app/administrador/listado-estudiantes/listado-estudiantes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EstudianteService } from "../../servicios/estudiante.servicio";
 
 @Component({
   selector: 'app-listado-estudiantes',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-estudiantes.component.css']
 })
 export class ListadoEstudiantesComponent implements OnInit {
+ public listadoEstudiantes;
+  constructor(public _estudianteServicio:EstudianteService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.listadoEstudiantes= await this._estudianteServicio.getListadoEstudiantes().toPromise();
+    console.log(JSON.parse(this.listadoEstudiantes.listadoEstudiantes));
   }
 
 }
