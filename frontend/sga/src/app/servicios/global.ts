c390f5f3536b04792e5859b744ed7a9399a81a10
stteffano13 <tefo.aguayo@gmail.com>
2021-07-06 09:35:25 -0500

    listo todas las fun cionalidades del sistema SGA empiezo integracion

--- frontend/sga/src/app/servicios/global.ts
+++ frontend/sga/src/app/servicios/global.ts
@@ -1,6 +1,6 @@
 export var GLOBAL = {
 // url: "http://138.197.102.175:3977/api/"
   //url: "http://www.appmontecarlotransvip.com:3977/api/"
-  url: "http://localhost:3977/api/"
-  // url: "https://sga.ebenezercolegio.edu.ec/api/"
+ // url: "http://localhost:3977/api/"
+   url: "https://sga.ebenezercolegio.edu.ec/api/"
 };
